aeaca2f51bb5bfebe71d22a14583c2603aa862ba
yutao <763824282@qq.com>
2020-05-23 17:28:25 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('starter test', () => {
+  const value = 5;
+  expect(value).toEqual(5);
+});
+
+test('starter test2', () => {
+  const value = 7;
+  expect(value).toEqual(7);
 });
