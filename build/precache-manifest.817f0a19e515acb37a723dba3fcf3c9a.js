31baaa4a7d9c93dde356dcc339da7bc5dcf1d4cd
yutao <763824282@qq.com>
2020-05-24 18:19:01 -0500

    update

---
+++ build/precache-manifest.817f0a19e515acb37a723dba3fcf3c9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6216f1a264611eb1169cf665d0f94f07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "543619b193528f509177",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e904ab48c85949d62ee",
+    "url": "/static/js/2.e6387eff.chunk.js"
+  },
+  {
+    "revision": "ffeafd0d335eb1566177528624b23c6e",
+    "url": "/static/js/2.e6387eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543619b193528f509177",
+    "url": "/static/js/main.b3e903f0.chunk.js"
+  },
+  {
+    "revision": "c6e6077863050e94f0d4",
+    "url": "/static/js/runtime-main.31d42c57.js"
+  }
+]);
