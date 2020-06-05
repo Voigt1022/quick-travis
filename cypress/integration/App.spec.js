b32db4312d301f611e6c384448f04d768aaa4ea6
yutao <763824282@qq.com>
2020-06-05 17:46:20 -0500

    test 2 & 3

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -3,4 +3,14 @@ describe('Test App', () => {
   it('launches', () => {
     cy.visit('/');
   });
+
+  it('make sure that login button appears', () => {
+    cy.visit('/');
+    cy.contains('Sign in with Google');
+  });
+
+  it('get new dialog when clicking "Sign in with Google" button', () => {
+    cy.visit('/');
+    cy.contains('Sign in with Google').click();
+  });
 });
